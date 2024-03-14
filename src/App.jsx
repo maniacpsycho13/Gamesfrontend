6cdffad207bfb435b2156c24a366fb9adaafb4fc
pankajra1 <pankajrai_21911@aitpune.edu.in>
2024-03-15 02:11:43 +0530

    added navbar to appjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-// src/App.js
 import React from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Importing Navbar
+import Navbar from './components/navbar';
 
 import Home from './components/Home';
 import LEVEL1 from './components/LEVEL-1';
@@ -11,12 +13,13 @@ import Puzzle from './components/LEVEL-3';
 import ImgReader from './components/LEVEL-4';
 import GuessPass from './components/LEVEL-5';
 import LEVEL6 from './components/LEVEL6';
+
 function App() {
   return (
-       
- 
-  <div>
-    <BrowserRouter>
+    <div>
+      <BrowserRouter>
+        {/* Insert Navbar outside of Routes but inside BrowserRouter */}
+        <Navbar />
         <Routes>
               <Route path = "/" element = {<Home/>}/>
               <Route path = "/LEVEL-1" element = {<LEVEL1/>}/>
@@ -25,12 +28,9 @@ function App() {
               <Route path = "/LEVEL-4" element = {<ImgReader/>}/>
               <Route path = "/LEVEL-5" element = {<GuessPass />} />
               <Route path = "/LEVEL-6" element = {<LEVEL6/>}/>
-
-              
         </Routes>
-    </BrowserRouter> 
-   
-  </div>
+      </BrowserRouter> 
+    </div>
   );
 }
 
